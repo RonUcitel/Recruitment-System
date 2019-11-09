0dd58b483023cb02e5140fd041aa0fd24e9e760a
ron ucitel <ron.ucitel@hotmail.com>
2019-11-09 23:00:48 +0200

    Add project files.

---
+++ Recruitment_System/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Recruitment_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox_PD = new System.Windows.Forms.GroupBox();
+            this.textBox_FirstName = new System.Windows.Forms.TextBox();
+            this.label_FirstName = new System.Windows.Forms.Label();
+            this.label_LastName = new System.Windows.Forms.Label();
+            this.textBox_LastName = new System.Windows.Forms.TextBox();
+            this.label_ID = new System.Windows.Forms.Label();
+            this.textBox_ID = new System.Windows.Forms.TextBox();
+            this.label_Email = new System.Windows.Forms.Label();
+            this.textBox_Email = new System.Windows.Forms.TextBox();
+            this.label_Phone = new System.Windows.Forms.Label();
+            this.textBox_Phone = new System.Windows.Forms.TextBox();
+            this.comboBox_Position = new System.Windows.Forms.ComboBox();
+            this.label_Position = new System.Windows.Forms.Label();
+            this.groupBox_Ranking = new System.Windows.Forms.GroupBox();
+            this.label_DBID = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button_Search = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.PDF_CV_Viewer = new AxAcroPDFLib.AxAcroPDF();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox_PD.SuspendLayout();
+            this.groupBox_Ranking.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PDF_CV_Viewer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox_PD
+            // 
+            this.groupBox_PD.Controls.Add(this.comboBox1);
+            this.groupBox_PD.Controls.Add(this.label_DBID);
+            this.groupBox_PD.Controls.Add(this.label_Position);
+            this.groupBox_PD.Controls.Add(this.comboBox_Position);
+            this.groupBox_PD.Controls.Add(this.label_Phone);
+            this.groupBox_PD.Controls.Add(this.textBox_Phone);
+            this.groupBox_PD.Controls.Add(this.label_Email);
+            this.groupBox_PD.Controls.Add(this.textBox_Email);
+            this.groupBox_PD.Controls.Add(this.label_ID);
+            this.groupBox_PD.Controls.Add(this.textBox_ID);
+            this.groupBox_PD.Controls.Add(this.label_LastName);
+            this.groupBox_PD.Controls.Add(this.textBox_LastName);
+            this.groupBox_PD.Controls.Add(this.label_FirstName);
+            this.groupBox_PD.Controls.Add(this.textBox_FirstName);
+            this.groupBox_PD.Location = new System.Drawing.Point(13, 13);
+            this.groupBox_PD.Name = "groupBox_PD";
+            this.groupBox_PD.Size = new System.Drawing.Size(731, 406);
+            this.groupBox_PD.TabIndex = 0;
+            this.groupBox_PD.TabStop = false;
+            this.groupBox_PD.Text = "Personal details";
+            // 
+            // textBox_FirstName
+            // 
+            this.textBox_FirstName.Location = new System.Drawing.Point(196, 74);
+            this.textBox_FirstName.Name = "textBox_FirstName";
+            this.textBox_FirstName.Size = new System.Drawing.Size(460, 44);
+            this.textBox_FirstName.TabIndex = 0;
+            this.textBox_FirstName.Text = "Ron";
+            // 
+            // label_FirstName
+            // 
+            this.label_FirstName.AutoSize = true;
+            this.label_FirstName.Location = new System.Drawing.Point(6, 77);
+            this.label_FirstName.Name = "label_FirstName";
+            this.label_FirstName.Size = new System.Drawing.Size(184, 37);
+            this.label_FirstName.TabIndex = 1;
+            this.label_FirstName.Text = "First Name:";
+            // 
+            // label_LastName
+            // 
+            this.label_LastName.AutoSize = true;
+            this.label_LastName.Location = new System.Drawing.Point(6, 127);
+            this.label_LastName.Name = "label_LastName";
+            this.label_LastName.Size = new System.Drawing.Size(182, 37);
+            this.label_LastName.TabIndex = 3;
+            this.label_LastName.Text = "Last Name:";
+            // 
+            // textBox_LastName
+            // 
+            this.textBox_LastName.Location = new System.Drawing.Point(196, 124);
+            this.textBox_LastName.Name = "textBox_LastName";
+            this.textBox_LastName.Size = new System.Drawing.Size(460, 44);
+            this.textBox_LastName.TabIndex = 2;
+            this.textBox_LastName.Text = "Ucitel";
+            // 
+            // label_ID
+            // 
+            this.label_ID.AutoSize = true;
+            this.label_ID.Location = new System.Drawing.Point(6, 177);
+            this.label_ID.Name = "label_ID";
+            this.label_ID.Size = new System.Drawing.Size(57, 37);
+            this.label_ID.TabIndex = 5;
+            this.label_ID.Text = "ID:";
+            // 
+            // textBox_ID
+            // 
+            this.textBox_ID.Location = new System.Drawing.Point(196, 174);
+            this.textBox_ID.Name = "textBox_ID";
+            this.textBox_ID.Size = new System.Drawing.Size(460, 44);
+            this.textBox_ID.TabIndex = 4;
+            this.textBox_ID.Text = "213102072";
+            // 
+            // label_Email
+            // 
+            this.label_Email.AutoSize = true;
+            this.label_Email.Location = new System.Drawing.Point(6, 227);
+            this.label_Email.Name = "label_Email";
+            this.label_Email.Size = new System.Drawing.Size(106, 37);
+            this.label_Email.TabIndex = 7;
+            this.label_Email.Text = "Email:";
+            // 
+            // textBox_Email
+            // 
+            this.textBox_Email.Location = new System.Drawing.Point(196, 224);
+            this.textBox_Email.Name = "textBox_Email";
+            this.textBox_Email.Size = new System.Drawing.Size(460, 44);
+            this.textBox_Email.TabIndex = 6;
+            this.textBox_Email.Text = "ron.ucitel@hotmail.com";
+            // 
+            // label_Phone
+            // 
+            this.label_Phone.AutoSize = true;
+            this.label_Phone.Location = new System.Drawing.Point(6, 277);
+            this.label_Phone.Name = "label_Phone";
+            this.label_Phone.Size = new System.Drawing.Size(118, 37);
+            this.label_Phone.TabIndex = 9;
+            this.label_Phone.Text = "Phone:";
+            // 
+            // textBox_Phone
+            // 
+            this.textBox_Phone.Location = new System.Drawing.Point(323, 274);
+            this.textBox_Phone.Name = "textBox_Phone";
+            this.textBox_Phone.Size = new System.Drawing.Size(333, 44);
+            this.textBox_Phone.TabIndex = 8;
+            this.textBox_Phone.Text = "5930503";
+            // 
+            // comboBox_Position
+            // 
+            this.comboBox_Position.FormattingEnabled = true;
+            this.comboBox_Position.Location = new System.Drawing.Point(196, 324);
+            this.comboBox_Position.Name = "comboBox_Position";
+            this.comboBox_Position.Size = new System.Drawing.Size(460, 45);
+            this.comboBox_Position.TabIndex = 10;
+            this.comboBox_Position.Text = "Manager";
+            // 
+            // label_Position
+            // 
+            this.label_Position.AutoSize = true;
+            this.label_Position.Location = new System.Drawing.Point(6, 327);
+            this.label_Position.Name = "label_Position";
+            this.label_Position.Size = new System.Drawing.Size(140, 37);
+            this.label_Position.TabIndex = 11;
+            this.label_Position.Text = "Position:";
+            // 
+            // groupBox_Ranking
+            // 
+            this.groupBox_Ranking.Controls.Add(this.pictureBox1);
+            this.groupBox_Ranking.Controls.Add(this.numericUpDown1);
+            this.groupBox_Ranking.Controls.Add(this.label1);
+            this.groupBox_Ranking.Location = new System.Drawing.Point(750, 13);
+            this.groupBox_Ranking.Name = "groupBox_Ranking";
+            this.groupBox_Ranking.Size = new System.Drawing.Size(731, 406);
+            this.groupBox_Ranking.TabIndex = 1;
+            this.groupBox_Ranking.TabStop = false;
+            this.groupBox_Ranking.Text = "Ranking";
+            // 
+            // label_DBID
+            // 
+            this.label_DBID.AutoSize = true;
+            this.label_DBID.Location = new System.Drawing.Point(6, 40);
+            this.label_DBID.Name = "label_DBID";
+            this.label_DBID.Size = new System.Drawing.Size(35, 37);
+            this.label_DBID.TabIndex = 12;
+            this.label_DBID.Text = "0";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(196, 274);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 45);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "050";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(112, 75);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(174, 44);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(345, 437);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(289, 94);
+            this.button_Save.TabIndex = 2;
+            this.button_Save.Text = "Save";
+            this.button_Save.UseVisualStyleBackColor = true;
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(712, 450);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(198, 69);
+            this.button_Clear.TabIndex = 3;
+            this.button_Clear.Text = "Clear";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            // 
+            // button_Search
+            // 
+            this.button_Search.Location = new System.Drawing.Point(26, 437);
+            this.button_Search.Name = "button_Search";
+            this.button_Search.Size = new System.Drawing.Size(237, 94);
+            this.button_Search.TabIndex = 4;
+            this.button_Search.Text = "Search";
+            this.button_Search.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 37;
+            this.listBox1.Location = new System.Drawing.Point(12, 551);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(465, 300);
+            this.listBox1.TabIndex = 5;
+            // 
+            // PDF_CV_Viewer
+            // 
+            this.PDF_CV_Viewer.Enabled = true;
+            this.PDF_CV_Viewer.Location = new System.Drawing.Point(1487, 13);
+            this.PDF_CV_Viewer.Name = "PDF_CV_Viewer";
+            this.PDF_CV_Viewer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("PDF_CV_Viewer.OcxState")));
+            this.PDF_CV_Viewer.Size = new System.Drawing.Size(707, 843);
+            this.PDF_CV_Viewer.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(271, 177);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 76);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2206, 868);
+            this.Controls.Add(this.PDF_CV_Viewer);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button_Search);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.groupBox_Ranking);
+            this.Controls.Add(this.groupBox_PD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox_PD.ResumeLayout(false);
+            this.groupBox_PD.PerformLayout();
+            this.groupBox_Ranking.ResumeLayout(false);
+            this.groupBox_Ranking.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PDF_CV_Viewer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_PD;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label_DBID;
+        private System.Windows.Forms.Label label_Position;
+        private System.Windows.Forms.ComboBox comboBox_Position;
+        private System.Windows.Forms.Label label_Phone;
+        private System.Windows.Forms.TextBox textBox_Phone;
+        private System.Windows.Forms.Label label_Email;
+        private System.Windows.Forms.TextBox textBox_Email;
+        private System.Windows.Forms.Label label_ID;
+        private System.Windows.Forms.TextBox textBox_ID;
+        private System.Windows.Forms.Label label_LastName;
+        private System.Windows.Forms.TextBox textBox_LastName;
+        private System.Windows.Forms.Label label_FirstName;
+        private System.Windows.Forms.TextBox textBox_FirstName;
+        private System.Windows.Forms.GroupBox groupBox_Ranking;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button_Search;
+        private System.Windows.Forms.ListBox listBox1;
+        private AxAcroPDFLib.AxAcroPDF PDF_CV_Viewer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
